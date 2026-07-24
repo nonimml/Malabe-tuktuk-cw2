25fc65093a03f4fc600235bf88a2ae09818f983e
nonim-dev <nonimrajasuriya@gmail.com>
2026-07-24 15:14:29 +0530

    Add FXML bindings to DealerViewController

--- src/main/java/com/cw_malabe_tutuk2/DealerViewController.java
+++ src/main/java/com/cw_malabe_tutuk2/DealerViewController.java
@@ -1,4 +1,14 @@
 package com.cw_malabe_tutuk2;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class DealerViewController {
+
+    @FXML private TableView<Dealer> DealerTable;
+    @FXML private TableColumn<Dealer, String> dealerContact;
+    @FXML private TableColumn<Dealer, String> dealerId;
+    @FXML private TableColumn<Dealer, String> dealerLocation;
+    @FXML private TableColumn<Dealer, String> dealerName;
 }
