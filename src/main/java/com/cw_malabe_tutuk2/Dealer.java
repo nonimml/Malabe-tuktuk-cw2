ac18597226191c2a95a802dce96a419a41fe394b
nonim-dev <nonimrajasuriya@gmail.com>
2026-07-24 15:10:19 +0530

    Add Dealer.java class with constructor and fields

--- src/main/java/com/cw_malabe_tutuk2/Dealer.java
+++ src/main/java/com/cw_malabe_tutuk2/Dealer.java
@@ -1,4 +1,15 @@
 package com.cw_malabe_tutuk2;
 
 public class Dealer {
+    private String supplierId;
+    private String name;
+    private String contactInfo;
+    private String location;
+
+    public Dealer(String supplierId, String name, String contactInfo,String location) {
+        this.setSupplierId(supplierId);
+        this.setName(name);
+        this.setContactInfo(contactInfo);
+        this.setLocation(location);
+    }
 }
