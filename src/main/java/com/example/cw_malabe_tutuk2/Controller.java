5764df652b943759d1dbbf93eac87c57f4387357
nonim-dev <nonimrajasuriya@gmail.com>
2026-07-24 14:20:32 +0530

    Initial commit

---
+++ src/main/java/com/example/cw_malabe_tutuk2/Controller.java
@@ -0,0 +1,14 @@
+package com.example.cw_malabe_tutuk2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/com/example/cw_malabe_tutuk2/Main.java
@@ -0,0 +1,9 @@
+package com.example.cw_malabe_tutuk2;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(MainApplication.class, args);
+    }
+}
