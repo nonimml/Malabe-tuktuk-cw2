f733d298ddf03d3582e550e08362c00bfd017094
nonim-dev <nonimrajasuriya@gmail.com>
2026-07-24 15:05:14 +0530

    Link new FXML layouts to controller for inventory and dealer

--- src/main/java/com/cw_malabe_tutuk2/MainApplication.java
+++ src/main/java/com/cw_malabe_tutuk2/MainApplication.java
@@ -1,6 +1,7 @@
 package com.cw_malabe_tutuk2;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -17,5 +18,30 @@ public class MainApplication extends Application {
         stage.setTitle("Malabe Spares Depot");
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    public UpdateInventoryController UpdateInventory(Product select) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("UpdateData.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1150, 625);
+        UpdateInventoryController controller = fxmlLoader.getController();
+        if(select != null){
+            controller.update(select,true);
+        }
+        Stage newStage = new Stage();
+        newStage.setTitle("Update Inventory Menu");
+        newStage.setScene(scene);
+        newStage.show();
+
+        return controller;
+    }
+
+    @FXML
+    public void ViewDealers() throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Dealers_Deatails.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        Stage newStage = new Stage();
+        newStage.setTitle("Dealers Details");
+        newStage.setScene(scene);
+        newStage.show();
     }
 }
