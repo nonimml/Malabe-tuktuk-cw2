7d01f1cc4d08421811a60822c6939e52f4487350
nonim-dev <nonimrajasuriya@gmail.com>
2026-07-24 16:19:31 +0530

    add ViewRandomDealers method to display the randomly sorted dealers

--- src/main/java/com/cw_malabe_tutuk2/DealerViewController.java
+++ src/main/java/com/cw_malabe_tutuk2/DealerViewController.java
@@ -1,8 +1,13 @@
 package com.cw_malabe_tutuk2;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
 
 public class DealerViewController {
 
@@ -11,4 +16,16 @@ public class DealerViewController {
     @FXML private TableColumn<Dealer, String> dealerId;
     @FXML private TableColumn<Dealer, String> dealerLocation;
     @FXML private TableColumn<Dealer, String> dealerName;
+
+    @FXML private void ViewRandomDealers(){
+        dealerId.setCellValueFactory(new PropertyValueFactory<>("supplierId"));
+        dealerContact.setCellValueFactory(new PropertyValueFactory<>("contactInfo"));
+        dealerName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        dealerLocation.setCellValueFactory(new PropertyValueFactory<>("location"));
+
+        fileHandler.dealerData(inventory);
+        List<Dealer> sortedlist = inventory.RandomDealers();
+        ObservableList<Dealer> observableList  = FXCollections.observableArrayList(sortedlist);
+        DealerTable.setItems(observableList);
+    }
 }
