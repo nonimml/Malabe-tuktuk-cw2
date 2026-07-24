744ecea69d5f5e7d6264ebb58e514c89c1e0e9f7
nonim-dev <nonimrajasuriya@gmail.com>
2026-07-24 15:30:03 +0530

    Add FXML TextField bindings for UpdateInventoryController

--- src/main/java/com/cw_malabe_tutuk2/UpdateInventoryController.java
+++ src/main/java/com/cw_malabe_tutuk2/UpdateInventoryController.java
@@ -1,4 +1,17 @@
 package com.cw_malabe_tutuk2;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class UpdateInventoryController {
+
+    @FXML private TextField brand;
+    @FXML private TextField codeField;
+    @FXML private TextField date;
+    @FXML private TextField imagePath;
+    @FXML private TextField name;
+    @FXML private TextField price;
+    @FXML private TextField stock;
+    @FXML private TextField type;
+    @FXML private TextField lowStock;
 }
