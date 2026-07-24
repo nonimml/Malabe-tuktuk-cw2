cacec6736da56dee69f77b059e765f9b5f5eae88
nonim-dev <nonimrajasuriya@gmail.com>
2026-07-24 15:18:52 +0530

    Add Inventory.java class with product, dealer, and cart lists

--- src/main/java/com/cw_malabe_tutuk2/Inventory.java
+++ src/main/java/com/cw_malabe_tutuk2/Inventory.java
@@ -1,4 +1,16 @@
 package com.cw_malabe_tutuk2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inventory {
+    private List<Product> products;
+    private  List<Dealer> dealers;
+    private  List<Product> cart;
+
+    public Inventory(){
+        this.dealers = new ArrayList<>();
+        this.products = new ArrayList<>();
+        this.cart = new ArrayList<>();
+    }
 }
